a7bf892e2d39f72d9c0ed09b116419a492bcac9f
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-10-30 23:56:52 +0200

    feat: add index.js to router (yet empty)

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  // {
+  //   path: "*",
+  //   name: "404",
+  //   component: NotFound,
+  // },
+];
+const router = new VueRouter({
+  routes
+})
+
+export default router
