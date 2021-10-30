85f39a1c5e8b5eb5c4873534c1a261f00e8ac1cf
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-10-30 23:55:52 +0200

    feat: add submit button as own component

---
+++ src/components/Partials/SubmitBtn.vue
@@ -0,0 +1,23 @@
+<template>
+    <button type="submit" @click.prevent="method">
+        Submit hier
+    </button>
+</template>
+
+<script>
+export default {
+    name: "submitbtn",
+    props: {
+        text: String,
+        method: {
+            type: Function,
+        }
+    },
+};
+</script>
+
+<style>
+button {
+    border: 1px solid red;
+}
+</style>
