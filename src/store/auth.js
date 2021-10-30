b6233e7d18014830a534337b6645f0d239583ed4
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-10-30 23:58:01 +0200

    feat: add auth.js to store

---
+++ src/store/auth.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+
+export default {
+
+    state: {
+        allStates: [
+            {
+              authenticated: null,
+              user: null,
+              errors: null,
+              success: null,
+              showSubmit: true,
+              loader: false,
+            },
+
+        ],
+    },
+
+    getters: {
+        authStates: state => state.allStates,
+    },
+
+    mutations: {
+      SET_AUTHENTICATED (state, value) {
+        state.allStates[0].authenticated = value
+      },
+      SET_USERTOKEN (state, value) {
+        state.allStates[0].user = value
+      },
+      SET_ERROR (state, value) {
+        state.allStates[0].errors = value
+      },
+      SET_SUCCESS (state, value) {
+        state.allStates[0].success = value
+      },
+      SET_SHOWSUBMIT (state, value) {
+        state.allStates[0].showSubmit = value
+      },
+      SET_LOADER (state, value) {
+        state.allStates[0].loader = value
+      }
+
+    },
+
+    actions: {
+        async login({ commit }, credentials) {
+            await axios.post("http://flow_backend.local/api/login", credentials).then((res) => {
+              commit('SET_AUTHENTICATED', true)
+              commit('SET_USERTOKEN', res.data)
+              commit('SET_SUCCESS', true)            
+              commit('SET_ERROR', false)            
+              commit('SET_LOADER', false)            
+              commit('SET_SHOWSUBMIT', true)            
+
+            }).catch(() => {
+              commit('SET_ERROR', true) 
+              commit('SET_LOADER', false)            
+              commit('SET_SHOWSUBMIT', true)            
+           
+            });
+        },
+        async logout({ commit }) {
+          await axios.get("http://flow_backend.local/api/logout").then(() => {
+            commit('SET_AUTHENTICATED', false)
+            commit('SET_USER', null)
+            commit('SET_SUCCESS', false)            
+
+          }).catch(() => {
+            commit('SET_ERROR', true)            
+          });
+      },
+
+    },
+};
