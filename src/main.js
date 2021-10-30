0effed84dcf823b5383d002c7a96647a5e2ef523
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-10-30 23:56:20 +0200

    feat: addaxios, router, store and moment to main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
+import axios from 'axios'
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import moment from 'moment'
+
 
 Vue.config.productionTip = false
+Vue.filter('formatDate', function(value) {
+  if (value) {
+    return moment(String(value)).format('DD.MM.YYYY')
+  }
+});
+
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
+
+
+axios.defaults.withCredentials = false
