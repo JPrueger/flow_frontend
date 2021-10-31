49e60c549d6a255b395734c5ec16550d2654a639
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-10-31 12:19:15 +0100

    feat: add view register

---
+++ src/views/Register.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Test Headline</h1>
+        <register-form />
+    </div>
+</template>
+
+<script>
+import RegisterForm from '../components/forms/Register-form.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RegisterForm,
+  }
+}
+</script>
