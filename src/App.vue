53df26fbfc84bf28decd20de4c927d21694f7110
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-10-31 12:18:34 +0100

    feat: add router view to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <div id="app">
-    <register-form />
+    <router-view />
   </div>
 </template>
 
 <script>
-import RegisterForm from './components/forms/Register-form.vue'
 
 export default {
   name: 'App',
-  components: {
-    RegisterForm
-  }
 }
 </script>
 
