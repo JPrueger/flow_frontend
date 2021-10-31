36780662ae9595eaba3db2626a1eb277daa93a2e
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-10-31 12:59:33 +0100

    feat: add login view

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Test Headline</h1>
+        <login-form />
+    </div>
+</template>
+
+<script>
+import LoginForm from '../components/forms/Login-form.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LoginForm,
+  }
+}
+</script>
