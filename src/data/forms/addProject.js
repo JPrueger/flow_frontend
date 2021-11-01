ff0287e08e6e3c8ef81f3f94b7ca91110a230f23
Kerstin Wukoschitz <kerstin.wukoschitz@compax.at>
2021-11-01 14:30:05 +0100

    defined input fields for addProject form

---
+++ src/data/forms/addProject.js
@@ -0,0 +1,14 @@
+const fields = [
+  {
+    name: "title",
+    type: "text",
+    label: "Title",
+    placeholder: "Your Project Title",
+    inputProps: {
+      required: true,
+      maxLength: 255,
+    },
+  },
+];
+
+export default fields;
