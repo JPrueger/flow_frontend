649a5ebd97a96c72dc96227b80edd17504527ecf
Kerstin Wukoschitz <kerstin.wukoschitz@compax.at>
2021-11-01 13:47:37 +0100

    add projectOverview with testdata

--- src/components/ColoredUserIcon.vue
+++ src/components/ColoredUserIcon.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="text-left">
     <div
-      class="rounded-lg bg-pink-700 py-1 px-3 text-white w-auto inline-block items-start"
+      class="rounded-lg py-1 px-3 text-white w-auto inline-block items-start"
+      :class="'bg-' + `${color}`"
     >
       {{ userName }}
     </div>
@@ -9,9 +10,6 @@
 </template>
 
 <script>
-// hier wird axios (http request library eingebunden), um die laravel endpunkte anzusteuern
-// import axios from "axios";
-
 export default {
   name: "ColoredUserIcon",
   data: () => ({
@@ -20,43 +18,7 @@ export default {
   }),
   props: {
     userName: String,
+    color: String,
   },
-  // ich verwende hier wieder localStorage, aber das Ganze würde natürlich auch mit Laravel funktionieren
-  //   methods: {
-  //     async saveTodo() {
-  //       const newTodoData = {
-  //         title: this.newTodo,
-  //         done: false,
-  //       };
-
-  //       this.todos.push(newTodoData);
-
-  //       try {
-  //         const dataEndpointUrl = "http://flow_backend.local/api/todos";
-  //         // bitte beachte hier -> wir verwenden axios.post (um einen POST request zu machen)
-  //         await axios.post(dataEndpointUrl, {
-  //           todo: newTodoData,
-  //         });
-
-  //         this.newTodo = "";
-
-  //         alert("Speichern erfolgreich");
-  //       } catch (error) {
-  //         alert("Es trat ein Fehler beim speichern auf: " + error);
-  //       }
-  //     },
-  //   },
-  //   async created() {
-  //     try {
-  //       const dataEndpointUrl = "http://flow_backend.local/api/todos";
-  //       // bitte beachte hier -> wir verwenden axios.get (um einen GET request zu machen)
-  //       const response = await axios.get(dataEndpointUrl);
-  //       console.log("MIKADO: ", response);
-
-  //       this.todos = response.data;
-  //     } catch (error) {
-  //       alert("Es trat ein Fehler beim speichern auf: " + error);
-  //     }
-  //   },
 };
 </script>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-white w-100 shadow-md rounded-sm mx-auto p-3">
+    <div class="text-left font-bold text-xl mb-1">{{ title }}</div>
+    <div class="text-left mb-5">{{ countTask }} Tasks</div>
+
+    <ul class="flex justify-between">
+      <li v-for="member in members" :key="member.userName">
+        <ColoredUserIcon :userName="member.userName" :color="member.color" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ColoredUserIcon from "./ColoredUserIcon.vue";
+
+export default {
+  name: "ProjectCard",
+  data: () => ({
+    newTodo: "",
+    todos: [],
+  }),
+  components: {
+    ColoredUserIcon,
+  },
+  props: {
+    title: String,
+    countTask: Number,
+    members: [],
+  },
+};
+</script>
