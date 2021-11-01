dac6c02763f1d1379f5d909ad24f367810562e36
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-11-01 13:54:22 +0100

    feat: add formfields email, password, text (done with kerstin)

---
+++ src/components/FormFields/PasswordField.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <label :for="name">{{ label }}</label>
+    <input
+      :id="name"
+      type="password"
+      ref="passwordField"
+      :placeholder="placeholder"
+      v-bind="inputProps"
+      :value="value"
+      v-on:input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: 'FormFieldsPasswordField',
+  props: {
+    value: {
+      required: true,
+      type: String,
+      default: '',
+    },
+    label: {
+      required: false,
+      type: String,
+    },
+    name: {
+      required: true,
+      type: String,
+    },
+    placeholder: {
+      required: false,
+      type: String,
+      default: '',
+    },
+    inputProps: {
+      required: false,
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
---
+++ src/components/FormFields/TextField.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <label :for="name">{{ label }}</label>
+    <input
+      :id="name"
+      type="text"
+      :placeholder="placeholder"
+      v-bind="inputProps"
+      :value="value"
+      v-on:input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: 'FormFieldsTextField',
+  props: {
+    value: {
+      required: true,
+      type: String,
+      default: '',
+    },
+    label: {
+      required: false,
+      type: String,
+    },
+    name: {
+      required: true,
+      type: String,
+    },
+    placeholder: {
+      required: false,
+      type: String,
+      default: '',
+    },
+    inputProps: {
+      required: false,
+      type: Object,
+      default: () => {},
+    },
+    icon: {
+      required: false,
+      type: String,
+    },
+  },
+};
+</script>
