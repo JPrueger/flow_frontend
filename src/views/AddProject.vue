12e9e9d9df5b4d199599b38184ec8b6f2682abd5
Kerstin Wukoschitz <kerstin.wukoschitz@compax.at>
2021-11-01 14:21:01 +0100

    add view for new project form

---
+++ src/views/AddProject.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <AddProjectForm />
+  </div>
+</template>
+
+<script>
+import AddProjectForm from "../components/Login/AddProjectForm.vue";
+
+export default {
+  name: "AddProject",
+  components: {
+    AddProjectForm,
+  },
+};
+</script>
