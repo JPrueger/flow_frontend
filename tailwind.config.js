35a4b2c9746dcfae295816fa5d19b08feb31914e
Kerstin Wukoschitz <kerstin.wukoschitz@compax.at>
2021-11-01 21:10:28 +0100

    add tailwind config for colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ module.exports = {
     fontFamily: {
       sans: ["Catamaran", "Arial", "sans-serif"],
     },
+    colors: {
+      mainText: "#212121",
+      pink: "#99154E",
+      yellow: "#FFB319",
+      blueGreen: {
+        light: "#49A6AA",
+        dark: "#0A474A",
+      },
+      black: "#000",
+      white: "#fff",
+      success: "#00FEAA",
+      successBackground: "#E5FFF6",
+      successDarker: "#00A26D",
+      error: "#FF4545",
+      errorBackground: "#FEEDED",
+      backgroundColor: "#6B61CC",
+    },
   },
   variants: {
     extend: {},
