57bbf28ee797d48f8f98e0e4282cfc6537d4923b
Kerstin Wukoschitz <kerstin.wukoschitz@compax.at>
2021-11-02 14:50:17 +0100

    adapt tasks and add testdata

--- src/views/ProjectBoard.vue
+++ src/views/ProjectBoard.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <h1 class="font-bold text-4xl mb-6">SAE BA-Project</h1>
     <div class="lg:flex lg:justify-around">
-      <BoardColumn columnName="Open" :list="todo" />
-      <BoardColumn columnName="In Progress" :list="doing" />
+      <BoardColumn columnName="Open" :list="openTasks" />
+      <BoardColumn columnName="In Progress" :list="inProgress" />
       <BoardColumn columnName="Done" :list="done" />
     </div>
   </div>
@@ -15,13 +15,46 @@ import BoardColumn from "@/components/Project/BoardColumn";
 export default {
   name: "ProjectBoard",
   components: {
-    BoardColumn,
+    BoardColumn
   },
   data: () => ({
-    newTodo: "",
-    todo: [{ name: "Wireframes" }, { name: "Screendesign" }],
-    doing: [],
-    done: [],
-  }),
+    openTasks: [
+      {
+        title: "Wireframes",
+        storyPoints: "5",
+        assignedUser: { userName: "Kersi", color: "blueGreen-light" }
+      },
+      {
+        title: "Screendesign",
+        storyPoints: "8",
+        assignedUser: { userName: "Jeannie", color: "yellow" }
+      },
+      {
+        title: "Wireframes",
+        storyPoints: "13",
+        assignedUser: { userName: "Juri", color: "blueGreen-dark" }
+      }
+    ],
+
+    inProgress: [
+      {
+        title: "Tailwind Config",
+        storyPoints: "5",
+        assignedUser: { userName: "Kersi", color: "blueGreen-light" }
+      },
+      {
+        title: "DB setup",
+        storyPoints: "8",
+        assignedUser: { userName: "Jeannie", color: "yellow" }
+      }
+    ],
+    done: [
+      {
+        title: "Projektplan",
+        storyPoints: "5",
+        assignedUser: { userName: "Danny", color: "pink" }
+      }
+    ]
+  })
 };
 </script>
