46aa371cf27735377dc5e6421e8ad9b32b82e08d
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-11-02 18:20:45 +0100

    feat: add functionality to saveProject form form to db

--- src/components/Project/AddProjectForm.vue
+++ src/components/Project/AddProjectForm.vue
@@ -1,7 +1,7 @@
 <template>
   <BaseForm
-    v-model="credentials"
-    @input="loginUser"
+    v-model="newProject"
+    @input="saveProject"
     headline="Add Project Headline"
     submitButton="Add Project"
     :fields="fields"
@@ -10,6 +10,7 @@
 <script>
 import addProjectFields from "@/data/forms/addProject.js";
 import BaseForm from "../BaseForm/BaseForm.vue";
+import axios from "axios";
 
 export default {
   components: {
@@ -17,13 +18,30 @@ export default {
   },
   data() {
     return {
-      credentials: {},
       loading: false,
       error: null,
       token: null,
       success: false,
       fields: addProjectFields,
+
+      newProject: {
+        title: "",
+      },
     };
+  },
+  methods: {
+    saveProject() {
+      axios
+        .post("http://flow_backend.local/api/add-project/create", this.newProject)
+        .then(() => {
+          alert("Speichern erfolgreich");
+        })
+        .catch((err) => {
+            alert("Speichern nicht erfolgreich");
+            this.errors = err.response.data.errors;
+            console.log(this.errors);
+        });
+    }
   },
 };
 </script>
