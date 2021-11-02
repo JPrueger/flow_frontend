e7a63bbd8ffe5070375d4e932ad9cf0d9c286624
Kerstin Wukoschitz <kerstin.wukoschitz@compax.at>
2021-11-02 14:49:55 +0100

    add testdata

--- src/views/ProjectsOverview.vue
+++ src/views/ProjectsOverview.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <h1 class="font-bold text-4xl mb-6">Projects</h1>
 
-    <ul class="md:flex">
-      <li v-for="project in projects" :key="project.title">
+    <ul class="md:flex justify-between flex-wrap">
+      <li v-for="project in projects" :key="project.title" class="w-1/4">
         <ProjectCard
           :title="project.title"
           :countTask="project.countTask"
@@ -20,7 +20,7 @@ import ProjectCard from "@/components/Project/ProjectCard";
 export default {
   name: "ProjectsOverview",
   components: {
-    ProjectCard,
+    ProjectCard
   },
   data: () => ({
     projects: [
@@ -28,31 +28,47 @@ export default {
         title: "Wireframes",
         countTask: "15",
         members: [
-          { userName: "Kersi", color: "green-700" },
-          { userName: "Jeannie", color: "yellow-400" },
-        ],
+          { userName: "Kersi", color: "blueGreen-light" },
+          { userName: "Jeannie", color: "yellow" }
+        ]
       },
       {
         title: "Screendesign",
         countTask: "28",
         members: [
-          { userName: "Kersi", color: "green-700" },
-          { userName: "Jeannie", color: "yellow-400" },
-          { userName: "Juri", color: "yellow-700" },
-        ],
+          { userName: "Kersi", color: "blueGreen-light" },
+          { userName: "Jeannie", color: "yellow" },
+          { userName: "Juri", color: "blueGreen-dark" }
+        ]
       },
       {
         title: "Testing",
         countTask: "5",
         members: [
-          { userName: "Jeannie", color: "yellow-400" },
-          { userName: "Danny", color: "pink-700" },
-        ],
+          { userName: "Jeannie", color: "yellow" },
+          { userName: "Danny", color: "pink" }
+        ]
       },
+      {
+        title: "Titan Killer Tour",
+        countTask: "5",
+        members: [
+          { userName: "Jeannie", color: "yellow" },
+          { userName: "Kersi", color: "blueGreen-light" }
+        ]
+      },
+      {
+        title: "Titan Killer Tour",
+        countTask: "5",
+        members: [
+          { userName: "Jeannie", color: "yellow" },
+          { userName: "Kersi", color: "blueGreen-light" }
+        ]
+      }
     ],
 
     doing: [],
-    done: [],
-  }),
+    done: []
+  })
 };
 </script>
