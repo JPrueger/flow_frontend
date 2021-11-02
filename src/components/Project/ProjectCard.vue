f86c4758fb3d75a00ac1cfe53d73cb98b3d10ee4
Kerstin Wukoschitz <kerstin.wukoschitz@compax.at>
2021-11-02 14:49:25 +0100

    adapt design for projectCard

--- src/components/Project/ProjectCard.vue
+++ src/components/Project/ProjectCard.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="bg-white w-100 shadow-md rounded-sm mx-auto p-3">
+  <div class="bg-white shadow-md rounded-sm mx-auto p-3">
     <div class="text-left font-bold text-xl mb-1">{{ title }}</div>
     <div class="text-left mb-5">{{ countTask }} Tasks</div>
 
     <ul class="flex justify-between">
-      <li v-for="member in members" :key="member.userName">
+      <li v-for="member in members" :key="member.userName" class="px-2">
         <ColoredUserIcon :userName="member.userName" :color="member.color" />
       </li>
     </ul>
@@ -18,15 +18,15 @@ export default {
   name: "ProjectCard",
   data: () => ({
     newTodo: "",
-    todos: [],
+    todos: []
   }),
   components: {
-    ColoredUserIcon,
+    ColoredUserIcon
   },
   props: {
     title: String,
     countTask: Number,
-    members: [],
-  },
+    members: []
+  }
 };
 </script>
