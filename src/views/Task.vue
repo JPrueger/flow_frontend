66a0571f150962452be16aa0aa8c29a22082c4e5
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-11-07 22:51:51 +0100

    feat: create view Task and get all Tasks via get request

---
+++ src/views/Task.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>Test Headline</h1>
+        <ul class="w-80">
+          <li v-for="task in tasks" v-bind:key="task.id">
+            <pre> {{ task }} </pre>
+          </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Task",
+    data: () => {
+      return {
+        tasks: "",
+      };
+    },
+  methods: {
+    getPost() {
+      axios
+        .get(
+          "http://flow_backend.local/api/tasks/index/"
+        )
+        .then((res) => {
+          this.tasks = res.data;
+      });
+    },
+  },
+  created() {
+      this.getPost();
+  },
+};
+</script>
