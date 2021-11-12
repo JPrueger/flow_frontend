75faddc43fb3c4fe12e90bb4bc65bc85a1077b55
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-11-12 20:11:41 +0100

    feat: add basic Navigation component (done with Kerstin)

---
+++ src/components/Navigation/Navigation.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <ul>
+            <li>
+                <router-link
+                    to="/"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="145" height="103"><text transform="translate(0 70)" fill="#212121" font-size="64" font-family="Catamaran-ExtraBold, Catamaran" font-weight="800"><tspan x="0" y="0">flow</tspan><tspan y="0" fill="#49a6aa">.</tspan></text></svg>
+                </router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+};
+</script>
