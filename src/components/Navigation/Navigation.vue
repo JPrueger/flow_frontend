65369ce572d17a1c18e9104339a7b871f6938106
kerstin <kerstin.wukoschitz@gmx.net>
2021-11-22 19:41:00 +0100

    adapt base styling

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -1,14 +1,30 @@
 <template>
-    <nav class="mb-8 xl:mb-4">
-        <ul>
-            <li>
+    <nav class="mb-8 xl:mb-4 xl:pb-5">
+        <div class="flex justify-between items-center">
+
                 <router-link
                     to="/"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="145" height="103"><text transform="translate(0 70)" fill="#212121" font-size="64" font-family="Catamaran-ExtraBold, Catamaran" font-weight="800"><tspan x="0" y="0">flow</tspan><tspan y="0" fill="#49a6aa">.</tspan></text></svg>
                 </router-link>
-            </li>
-        </ul>
+
+          <div class="flex">
+
+              <router-link
+                  to="/project-board"
+              >
+                Dashboard
+              </router-link>
+
+
+              <router-link class="mx-4"
+                  to="/user-profile"
+              >
+                Profile
+              </router-link>
+
+          </div>
+        </div>
     </nav>
 </template>
 
