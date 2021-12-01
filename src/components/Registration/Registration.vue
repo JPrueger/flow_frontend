67d0fb365326135c7b0baab912acf802bead94de
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-12-01 22:37:23 +0100

    feat: add new register form to view

---
+++ src/components/Registration/Registration.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="flex">
+      <RegisterForm
+        v-model="newUser"
+        @input="saveUser"
+        headline="Hey there!"
+        subheadline="Good seeing you. Tell us your details and choose a character to get you started into flow."
+        submitButton="Submit!"
+        :responseState="response"
+        :fields="fields"
+        class="mb-4"
+      />
+    </div>
+    <p class="text-center">
+      Already registered?
+      <router-link to="/login" class="text-center Link">Login here!</router-link>
+    </p>
+  </div>
+</template>
+<script>
+import registerFields from "@/data/forms/registration.js";
+import axios from "axios";
+import RegisterForm from "../RegisterForm/RegisterForm.vue";
+
+export default {
+  components: {
+    RegisterForm,
+  },
+  data() {
+    return {
+      formData: {},
+      response: {},
+      fields: registerFields,
+      newUser: {
+        name: "",
+        email: "",
+        password: "",
+      },
+      errors: [],
+      succesSignin: false,
+      activeSlide: 1
+    };
+  },
+  computed: {
+    styleObject: function() {
+      const width = 0 - ((this.activeSlide-1) * 400)
+      return {
+        transform: 'translateX('+width+'px)'
+      }
+    }
+  },
+  methods: {
+    saveUser() {
+      console.log("clicked");
+      axios
+        .post("http://flow_backend.local/api/user/register", this.newUser)
+        .then((response) => {
+          console.log("test hier rein?", this.newUser);
+          console.log("noch mal mikado: ", response);
+          alert("Speichern erfolgreich");
+        })
+        .then(() => {
+          this.succesSignin = true;
+        })
+        .catch((err) => {
+          alert("Speichern nicht erfolgreich");
+          this.errors = err.response.data.errors;
+          console.log(this.errors);
+        });
+    },
+    changeSlide: function(num) {
+      this.activeSlide = num
+    },
+    nextSlide: function() { 
+      if(this.activeSlide < 2) this.activeSlide++
+    },
+    prevSlide: function() {
+      if(this.activeSlide > 1) this.activeSlide--
+    }
+  },
+};
+</script>
