146e6621c4affd31296acf40df906bc119f20d30
kerstin <kerstin.wukoschitz@gmx.net>
2021-12-02 17:52:34 +0100

    show projects from user

--- src/views/ProjectsOverview.vue
+++ src/views/ProjectsOverview.vue
@@ -4,10 +4,14 @@
 
     <ul class="md:flex justify-between flex-wrap">
       <li v-for="project in projects" :key="project.title" class="w-1/4">
+<!--        <ProjectCard-->
+<!--          :title="project.title"-->
+<!--         :countTask="project.tasks.size()"-->
+<!--          :members="project.members"-->
+<!--        />-->
         <ProjectCard
-          :title="project.title"
-          :countTask="project.countTask"
-          :members="project.members"
+            :title="project.title"
+            :members="project.members"
         />
       </li>
     </ul>
@@ -15,60 +19,95 @@
 </template>
 
 <script>
+
+import axios from "axios";
 import ProjectCard from "@/components/Project/ProjectCard";
+import userDataService from "@/services/userDataService";
+
 
 export default {
   name: "ProjectsOverview",
   components: {
     ProjectCard
   },
-  data: () => ({
-    projects: [
-      {
-        title: "Wireframes",
-        countTask: "15",
-        members: [
-          { userName: "Kersi", color: "blueGreen-light" },
-          { userName: "Jeannie", color: "yellow" }
-        ]
+  data: () => {
+    return {
+      projects: "",
+      userId: "",
+    };
+  },
+  methods: {
+    getPost() {
+      axios
+          .get(
+              "http://flow_backend.local/api/projects/" + this.userId
+          )
+          .then((res) => {
+            this.projects = res.data;
+          });
       },
-      {
-        title: "Screendesign",
-        countTask: "28",
-        members: [
-          { userName: "Kersi", color: "blueGreen-light" },
-          { userName: "Jeannie", color: "yellow" },
-          { userName: "Juri", color: "blueGreen-dark" }
-        ]
-      },
-      {
-        title: "Testing",
-        countTask: "5",
-        members: [
-          { userName: "Jeannie", color: "yellow" },
-          { userName: "Danny", color: "pink-main" }
-        ]
-      },
-      {
-        title: "Titan Killer Tour",
-        countTask: "5",
-        members: [
-          { userName: "Jeannie", color: "yellow" },
-          { userName: "Kersi", color: "blueGreen-light" }
-        ]
-      },
-      {
-        title: "Titan Killer Tour",
-        countTask: "5",
-        members: [
-          { userName: "Jeannie", color: "yellow" },
-          { userName: "Kersi", color: "blueGreen-light" }
-        ]
-      }
-    ],
-
-    doing: [],
-    done: []
-  })
+  },
+  created() {
+    userDataService.me().then((userData) => {
+      this.userId = userData.id;
+      this.getPost();
+    });
+  },
 };
+
+
+// export default {
+//   name: "ProjectsOverview",
+//   components: {
+//     ProjectCard
+//   },
+//   data: () => ({
+//     projects: [
+//       {
+//         title: "Wireframes",
+//         countTask: "15",
+//         members: [
+//           { userName: "Kersi", color: "blueGreen-light" },
+//           { userName: "Jeannie", color: "yellow" }
+//         ]
+//       },
+//       {
+//         title: "Screendesign",
+//         countTask: "28",
+//         members: [
+//           { userName: "Kersi", color: "blueGreen-light" },
+//           { userName: "Jeannie", color: "yellow" },
+//           { userName: "Juri", color: "blueGreen-dark" }
+//         ]
+//       },
+//       {
+//         title: "Testing",
+//         countTask: "5",
+//         members: [
+//           { userName: "Jeannie", color: "yellow" },
+//           { userName: "Danny", color: "pink-main" }
+//         ]
+//       },
+//       {
+//         title: "Titan Killer Tour",
+//         countTask: "5",
+//         members: [
+//           { userName: "Jeannie", color: "yellow" },
+//           { userName: "Kersi", color: "blueGreen-light" }
+//         ]
+//       },
+//       {
+//         title: "Titan Killer Tour",
+//         countTask: "5",
+//         members: [
+//           { userName: "Jeannie", color: "yellow" },
+//           { userName: "Kersi", color: "blueGreen-light" }
+//         ]
+//       }
+//     ],
+//
+//     doing: [],
+//     done: []
+//   })
+// };
 </script>
