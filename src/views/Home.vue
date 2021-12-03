5f10a7ec5a21264d0781e6dda7e0218737059b66
Jeannine Prueger <jeannine.prueger@gmail.com>
2021-12-03 22:31:16 +0100

    fix: adjust general home and app styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,23 @@
 <template>
-  <div class="xl:pl-16 xl:flex justify-between md:w-3/4 xl:w-full">
+  <div class="xl:flex justify-between">
     <div class="xl:w-2/4 xl:mr-10">
-      <h1 class="font-bold mb-6 ">A fun way to manage
-        your projects.</h1>
+      <h1 class="font-bold my-6">Manage your projects.</h1>
       <p class="mb-6">Flow is a powerful tool to manage your projects in a still simple way. First choose your character and see it’s growth every time you finish up a ticket. All you need to do first is register and we’ll help you to keep your projects going.</p>
       <p class="mb-6">Flow is a powerful tool to manage your projects in a still simple way. First choose your character and see it’s growth every time you finish up a ticket. All you need to do first is register and we’ll help you to keep your projects going.</p>
-      <p class="mb-6">Flow is a powerful tool to manage your projects in a still simple way. First choose your character and see it’s growth every time you finish up a ticket. All you need to do first is register and we’ll help you to keep your projects going.</p>
-      <p class="mb-6">Flow is a powerful tool to manage your projects in a still simple way. First choose your character and see it’s growth every time you finish up a ticket. All you need to do first is register and we’ll help you to keep your projects going.</p>
-
-    <CtaLink />
+      <CtaLink />
     </div>
-
-    <img class="invisible lg:visible xl:w-2/3 md:mt-6 xl:pt-16" src="../assets/images/home/home_header_img.png" alt="preview of the project board">
+    <div>
+      <img class="invisible lg:visible md:mt-6" src="../assets/images/home/home_header_img.png" alt="preview of the project board">
+    </div>
   </div>
 </template>
 
 <script>
-
-
-
 import CtaLink from "../components/Partials/CtaLink";
 
 export default {
+  watch: {
+  },
   name: "Home",
   components: {CtaLink}
 }
