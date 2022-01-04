cb7801c14a4aeb6140a324dfe2d9e61daa85f35d
Jeannine Prueger <jeannine.prueger@gmail.com>
2022-01-05 00:00:17 +0100

    feat: add edit svg, format userprofile doc, show character according to collected storypoints

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="max-w-3xl mx-auto">
-<!--    <pre>-->
-<!--      {{ userData }}-->
-<!--    </pre>-->
     <Loader v-if="loader" />
     <div v-if="!loader">
       <div class="flex justify-between">
@@ -10,12 +7,25 @@
           <h1>Profile</h1>
         </div>
         <router-link :to="'/user/edit/' + userId">
-          Edit
+          <img src="@/assets/images/edit.svg" class="mt-1">
         </router-link>
       </div>
-      <div class="md:flex justify-between shadow p-6 rounded md:h-80 relative wrapper-profile">
+      <div
+        class="
+          md:flex
+          justify-between
+          shadow
+          p-6
+          rounded
+          md:h-80
+          relative
+          wrapper-profile
+        "
+      >
         <div class="md:flex justify-between">
-          <div class="text-left flex flex-col justify-between md:mr-10 lg:mr-12">
+          <div
+            class="text-left flex flex-col justify-between md:mr-10 lg:mr-12"
+          >
             <div>
               <h2>Username:</h2>
               <p class="mb-4">{{ userData.name }}</p>
@@ -24,8 +34,12 @@
             </div>
             <div class="mt-4 md:mt-0">
               <h2>Current Storypoints:</h2>
-              <p v-if="userData.storypoints === null" class="text-lg font-bold">No Storypoints collected yet.</p>
-              <p v-else class="text-lg font-bold">{{ userData.storypoints }} SP</p>
+              <p v-if="userData.storypoints === null" class="text-lg font-bold">
+                No Storypoints collected yet.
+              </p>
+              <p v-else class="text-lg font-bold">
+                {{ userData.storypoints }} SP
+              </p>
             </div>
           </div>
           <div class="flex justify-end h-auto flex-col relative">
@@ -34,15 +48,63 @@
                 class="mt-4 Video"
                 ref="player"
                 :options="options"
-                :video-id="648594265"
+                :video-id="videoId"
                 :player-height="height"
               />
-              <!-- <video class="w-52-why" width="380" height="240" autoplay loop>
-                <source src="@/assets/videos/dragon-1.mp4" type="video/mp4">
-              </video> -->
             </div>
           </div>
-          <svg class="absolute -bottom-7 -left-7 form-shape" xmlns="http://www.w3.org/2000/svg" width="70.47" height="68.074"><g data-name="Gruppe 61" fill="none" stroke="#ffb319" stroke-linecap="round" stroke-linejoin="round" stroke-width="5"><g data-name="Gruppe 37"><path data-name="Pfad 27" d="M56.139 32.711c5.135 4.624-3.95 12.932 2.603 18.832"/><path data-name="Pfad 28" d="M34.533 13.258c5.136 4.623-3.948 12.932 2.603 18.831"/><path data-name="Pfad 29" d="M56.139 32.711c-5.136-4.624-12.45 5.278-19.003-.622"/><path data-name="Pfad 31" d="M34.533 13.258c-5.135-4.624-12.448 5.279-19.001-.622"/></g><g data-name="Gruppe 38"><path data-name="Pfad 27" d="M44.139 45.711c5.135 4.624-3.95 12.932 2.603 18.832"/><path data-name="Pfad 28" d="M22.533 26.258c5.136 4.623-3.948 12.932 2.603 18.831"/><path data-name="Pfad 29" d="M44.139 45.711c-5.136-4.624-12.45 5.278-19.003-.622"/><path data-name="Pfad 31" d="M22.533 26.258c-5.135-4.624-12.448 5.279-19.001-.622"/></g></g></svg>
+          <svg
+            class="absolute -bottom-7 -left-7 form-shape"
+            xmlns="http://www.w3.org/2000/svg"
+            width="70.47"
+            height="68.074"
+          >
+            <g
+              data-name="Gruppe 61"
+              fill="none"
+              stroke="#ffb319"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="5"
+            >
+              <g data-name="Gruppe 37">
+                <path
+                  data-name="Pfad 27"
+                  d="M56.139 32.711c5.135 4.624-3.95 12.932 2.603 18.832"
+                />
+                <path
+                  data-name="Pfad 28"
+                  d="M34.533 13.258c5.136 4.623-3.948 12.932 2.603 18.831"
+                />
+                <path
+                  data-name="Pfad 29"
+                  d="M56.139 32.711c-5.136-4.624-12.45 5.278-19.003-.622"
+                />
+                <path
+                  data-name="Pfad 31"
+                  d="M34.533 13.258c-5.135-4.624-12.448 5.279-19.001-.622"
+                />
+              </g>
+              <g data-name="Gruppe 38">
+                <path
+                  data-name="Pfad 27"
+                  d="M44.139 45.711c5.135 4.624-3.95 12.932 2.603 18.832"
+                />
+                <path
+                  data-name="Pfad 28"
+                  d="M22.533 26.258c5.136 4.623-3.948 12.932 2.603 18.831"
+                />
+                <path
+                  data-name="Pfad 29"
+                  d="M44.139 45.711c-5.136-4.624-12.45 5.278-19.003-.622"
+                />
+                <path
+                  data-name="Pfad 31"
+                  d="M22.533 26.258c-5.135-4.624-12.448 5.279-19.001-.622"
+                />
+              </g>
+            </g>
+          </svg>
         </div>
       </div>
     </div>
@@ -70,6 +132,7 @@ export default {
         controls: false,
         loop: true,
       },
+      videoId: "",
     };
   },
   methods: {
@@ -82,10 +145,37 @@ export default {
         })
         .then(() => {
           this.loader = false;
-        })
+        });
     },
   },
-  created() {
+  beforeUpdate() {
+    // ghost
+    if (this.userData.character_id === 2) {
+      if (this.userData.storypoints >= 5 && this.userData.storypoints <= 9) {
+        this.videoId = "662431051";
+      } else if (
+        this.userData.storypoints >= 10 &&
+        this.userData.storypoints <= 14
+      ) {
+        this.videoId = "662431018";
+      } else if (this.userData.storypoints >= 15) {
+        this.videoId = "662430984";
+      }
+      // dragon
+    } else if (this.userData.character_id === 1) {
+      if (this.userData.storypoints >= 5 && this.userData.storypoints <= 9) {
+        this.videoId = "662430956";
+      } else if (
+        this.userData.storypoints >= 10 &&
+        this.userData.storypoints <= 14
+      ) {
+        this.videoId = "662430924";
+      } else if (this.userData.storypoints >= 15) {
+        this.videoId = "662430854";
+      }
+    }
+  },
+  mounted() {
     userDataService.me().then((userData) => {
       this.userId = userData.id;
       this.getUserDetails();
@@ -106,7 +196,7 @@ export default {
 .Video {
   width: 200px;
   position: absolute;
-  right: -50px;
+  right: -20px;
   top: 5px;
   @apply bottom-8 -z-10;
 }
