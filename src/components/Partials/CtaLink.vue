c7e50d93320ad8dd708ed646d2ea891130ac2908
kerstin <kerstin.wukoschitz@gmx.net>
2022-01-10 19:15:39 +0100

    bugfix for 'add project' link in home view

--- src/components/Partials/CtaLink.vue
+++ src/components/Partials/CtaLink.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="Button">
-    <router-link to="/add-project"> Start your Project </router-link>
+    <router-link :to="userData ? '/add-project' : '/register'"> Start your Project</router-link>
   </div>
 </template>
 
 <script>
+import userDataService from "@/services/userDataService";
+
 export default {
   name: "CtaLink",
+  data: () => {
+    return {
+      userData: null,
+    };
+  },
+  created() {
+    userDataService.me().then((userData) => {
+      this.userData = userData;
+    });
+  },
 };
 </script>
