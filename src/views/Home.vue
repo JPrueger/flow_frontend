28a2a290c095bf8ee98eaefddf33ea54ee24a8ec
kerstin <kerstin.wukoschitz@gmx.net>
2022-01-11 16:49:17 +0100

    embed new awesome video for home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="xl:flex justify-between">
-    <div class="xl:w-2/4 xl:mr-10">
+    <div class="xl:w-1/3 xl:mr-10">
       <h1 class="font-bold my-6">Manage your projects.</h1>
       <p class="mb-6">
         <span class="font-bold">flow<span class="font-bold text-xl text-pink-main">.</span></span> is a powerful tool to manage your projects in a still simple way.
@@ -10,12 +10,19 @@
       </p>
       <CtaLink />
     </div>
-    <div>
-      <img
-        class="invisible lg:visible md:mt-6"
-        src="../assets/images/home/home_header_img.png"
-        alt="preview of the project board"
+    <div class="xl:w-100 flex justify-end">
+      <vimeo-player
+          ref="player"
+          :options="options"
+          :video-id="664821191"
+          :player-height="height"
+          :player-width="width"
       />
+<!--      <img-->
+<!--        class="invisible lg:visible md:mt-6"-->
+<!--        src="../assets/images/home/home_header_img.png"-->
+<!--        alt="preview of the project board"-->
+<!--      />-->
     </div>
   </div>
 </template>
@@ -25,6 +32,19 @@ import CtaLink from "../components/Partials/CtaLink";
 
 export default {
   name: "Home",
+  data: () => ({
+    height: 550,
+    width: 1000,
+    // see options here: https://www.npmjs.com/package/vue-vimeo-player
+    options: {
+      muted: true,
+      autoplay: true,
+      controls: false,
+      loop: true,
+    },
+    videoSource: "",
+    userData: "",
+  }),
   components: { CtaLink },
 };
 </script>
